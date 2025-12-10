8999e1cd0113832cba3bfc98c08bcf534f37904c
Tallal64 <firefoxtallal@gmail.com>
2025-12-10 13:17:58 +0500

    feat: integrate next-auth for user authentication with session management and email verification

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,26 @@
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+  interface User {
+    _id?: string;
+    username?: string;
+    isVerified?: boolean;
+    isAcceptingMessages?: boolean;
+  }
+
+  interface Session {
+    user: {
+      _id?: string;
+      username?: string;
+      isVerified?: boolean;
+      isAcceptingMessages?: boolean;
+    } & DefaultSession["user"];
+  }
+
+  interface JWT {
+    _id?: string;
+    username?: string;
+    isVerified?: boolean;
+    isAcceptingMessages?: boolean;
+  }
+}
